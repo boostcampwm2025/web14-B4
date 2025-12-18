4753e0c6f7a1c548a035aa1ef9cae9d094121d9c
standard-chan <starp321@naver.com>
2025-12-18 15:55:27 +0900

    chore: 환경변수용 nestjs/config 패키지 설치
    - 패키기 설치
    - app module에 config 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecordsModule } from './modules/records/records.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RecordsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 전역 사용
+    }),
+    RecordsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
