84acaed5e1f6ab467989a341e9aaa15dfbf51fe3
jum0624 <jum0624@naver.com>
2026-01-05 23:52:47 +0900

    style: commitlint config 포맷 수정

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,21 +4,12 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'test',
-        'chore',
-        'hotfix'    
-      ]
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'hotfix'],
     ],
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 100]
-  }
+    'header-max-length': [2, 'always', 100],
+  },
 };
