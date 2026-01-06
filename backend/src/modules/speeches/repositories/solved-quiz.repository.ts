c6553e907fdadd884c671891ae539b5dc0f5ae95
standard-chan <starp321@naver.com>
2026-01-06 17:31:51 +0900

    feat: solved quiz repository 구현
    - 구현함 함수 : 생성, 메인 퀴즈 id로 조회, 음성 텍스트 업데이트, id로
      찾기

---
+++ backend/src/modules/speeches/repositories/solved-quiz.repository.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { SolvedQuiz } from '../entities/solved-quiz.entity';
+
+@Injectable()
+export class SolvedQuizRepository extends Repository<SolvedQuiz> {
+  constructor(private dataSource: DataSource) {
+    super(SolvedQuiz, dataSource.createEntityManager());
+  }
+
+  async createSolvedQuiz(
+    userId: number,
+    mainQuizId: number,
+    speechText: string,
+  ): Promise<SolvedQuiz> {
+    const solvedQuiz = this.create({
+      userId,
+      mainQuizId,
+      speechText,
+    });
+
+    return await this.save(solvedQuiz);
+  }
+
+  // 최신 순으로 조회
+  async findByMainQuizId(mainQuizId: number): Promise<SolvedQuiz[]> {
+    return await this.find({
+      where: { mainQuizId },
+      order: { createdAt: 'DESC' },
+    });
+  }
+
+  async updateSpeechText(
+    solvedQuizId: number,
+    speechText: string,
+  ): Promise<SolvedQuiz> {
+    const solvedQuiz = await this.findById(solvedQuizId);
+
+    // TODO : 별도 ERROR class 생성하여 부여 필요
+    if (!solvedQuiz) throw new Error('SolvedQuizId가 존재하지 않습니다.');
+
+    solvedQuiz.speechText = speechText;
+
+    return await this.save(solvedQuiz);
+  }
+
+  async findById(solvedQuizId: number): Promise<SolvedQuiz | null> {
+    return await this.findOne({ where: { solvedQuizId } });
+  }
+}
