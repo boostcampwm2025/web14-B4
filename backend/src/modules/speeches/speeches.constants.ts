fc0da50c65789de02b6e41b41c4af1e1e5736536
standard-chan <starp321@naver.com>
2026-01-06 16:36:29 +0900

    refactor: 도메인 이름을 records -> speeches로 변경 및 lint 에러 수정
    - 타입 lint 오류 해결을 위해 record를 buffer 타입으로 명시적으로 지정
    - lint 오류 해결을 위해 테스트 코드를 임시 테스트로 변경

---
+++ backend/src/modules/speeches/speeches.constants.ts
@@ -0,0 +1,14 @@
+// 녹음 파일 MIME 타입
+export const allowedMimeTypes = [
+  'audio/wav',
+  'audio/webm',
+  'audio/mpeg',
+  'audio/mp3',
+  'audio/ogg',
+];
+
+// 외부 url
+export const CLOVA_STT = {
+  BASE_URL: 'https://naveropenapi.apigw.ntruss.com/recog/v1/stt',
+  DEFAULT_LANG: 'Kor',
+};
---
+++ backend/src/modules/speeches/speeches.service.spec.ts
@@ -0,0 +1,6 @@
+// lint 에러 발생으로 인해, 임시 테스트 코드 작성
+describe('dummy', () => {
+  it('passes', () => {
+    expect(true).toBe(true);
+  });
+});
