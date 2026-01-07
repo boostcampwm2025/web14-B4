984ba4f0fe4351e2ef76e2a01dc0d564dd59005f
jum0624 <jum0624@naver.com>
2026-01-07 16:14:12 +0900

    feat: tbMainQuiz 엔티티 생성

---
+++ backend/src/quizzes/entities/tb-main-quiz.entity.ts
@@ -0,0 +1,40 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { TbChecklistItem } from './tb-checklist-item.entity';
+
+@Entity('tb_main_quiz')
+export class TbMainQuiz {
+  @PrimaryGeneratedColumn({ name: 'main_quiz_id' })
+  mainQuizId: number;
+
+  @Column({ name: 'quiz_category_id' })
+  quizCategoryId: number;
+
+  @Column({ name: 'difficulty_level', length: 10 })
+  difficultyLevel: string;
+
+  @Column({ name: 'title', length: 255 })
+  title: string;
+
+  @Column({ name: 'content', length: 255 })
+  content: string;
+
+  @Column({ name: 'hint', length: 255, nullable: true })
+  hint: string;
+
+  @Column({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @Column({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @OneToMany(() => TbChecklistItem, (item) => item.mainQuiz)
+  checklistItems: TbChecklistItem[];
+}
