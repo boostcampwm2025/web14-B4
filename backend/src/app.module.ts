02a843274ba2fd6fc43426d028ff34dd948c5787
Hyerin Park <phl2510@naver.com>
2026-01-07 23:52:47 +0900

    fix: dev 브랜치 작업 내용 반영 중 발생한 충돌 해결

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,13 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecordsModule } from './modules/records/records.module';
-<<<<<<< HEAD
-import { ConfigModule } from '@nestjs/config';
-import { QuizModule } from './modules/quizzes/quiz.module';
-=======
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
->>>>>>> 3b709fc93bed58754979bafa68d5a2d20807bb2b
 
 @Module({
   imports: [
@@ -30,7 +25,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       inject: [ConfigService],
     }),
     RecordsModule,
-    QuizModule,
   ],
   controllers: [AppController],
   providers: [AppService],
