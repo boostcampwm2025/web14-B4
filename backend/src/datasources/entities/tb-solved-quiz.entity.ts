0bd02553be42c93cfd1e5f9ae9f72332a6acce6d
standard-chan <starp321@naver.com>
2026-01-12 16:19:55 +0900

    feat: solvedQuiz에 이해도 컬럼 추가
    - User는 추후 추가할 때, 주석 해제

--- backend/src/datasources/entities/tb-solved-quiz.entity.ts
+++ backend/src/datasources/entities/tb-solved-quiz.entity.ts
@@ -8,6 +8,12 @@ import {
 } from 'typeorm';
 import { MainQuiz } from './tb-main-quiz.entity';
 
+export enum ComprehensionLevel {
+  HIGH = 'HIGH',
+  NORMAL = 'NORMAL',
+  LOW = 'LOW',
+}
+
 @Entity('tb_solved_quiz')
 export class SolvedQuiz {
   @PrimaryGeneratedColumn('increment', {
@@ -24,9 +30,21 @@ export class SolvedQuiz {
   @JoinColumn({ name: 'main_quiz_id' })
   mainQuiz: MainQuiz;
 
+  // @ManyToOne(() => User, { nullable: false })
+  // @JoinColumn({ name: 'user_id' })
+  // user: User;
+
   @Column('text', { name: 'speech_text' })
   speechText: string;
 
-  @CreateDateColumn()
+  @Column({
+    name: 'comprehension_level',
+    type: 'enum',
+    enum: ComprehensionLevel,
+    nullable: true,
+  })
+  comprehensionLevel?: ComprehensionLevel;
+
+  @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 }
