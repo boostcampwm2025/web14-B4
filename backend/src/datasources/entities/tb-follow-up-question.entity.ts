522fdc1b7c609df00b563337396e3eaae4447463
standard-chan <starp321@naver.com>
2026-01-12 16:44:58 +0900

    feat: 꼬리질문 entity 추가
    - 특이사항 없음

---
+++ backend/src/datasources/entities/tb-follow-up-question.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { MainQuiz } from './tb-main-quiz.entity';
+
+@Entity('tb_follow_up_question')
+export class FollowUpQuestion {
+  @PrimaryGeneratedColumn('increment', {
+    type: 'bigint',
+    name: 'follow_up_question_id',
+  })
+  followUpQuestionId: number;
+
+  @ManyToOne(() => MainQuiz, { nullable: false })
+  @JoinColumn({ name: 'main_quiz_id' })
+  mainQuiz: MainQuiz;
+
+  @Column({ name: 'content', type: 'varchar', length: 255, nullable: true })
+  content?: string;
+}
