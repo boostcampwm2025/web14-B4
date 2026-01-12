8dedba31b21d5712aa47a0b748473ace5b5cf0ac
standard-chan <starp321@naver.com>
2026-01-12 16:47:18 +0900

    feat: flash card entity 추가
    - content, answer 은 nullable true 로 설정. ERD 대로 구현함
    - 해당 값은 DB상에 심각한 오류를 불러일으키지 않으므로 그렇게 한것으로
      기억

---
+++ backend/src/datasources/entities/tb-flashcard.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { MainQuiz } from './tb-main-quiz.entity';
+
+@Entity('tb_flashcard')
+export class Flashcard {
+  @PrimaryGeneratedColumn('increment', {
+    type: 'bigint',
+    name: 'flashcard_id',
+  })
+  flashcardId: number;
+
+  @ManyToOne(() => MainQuiz, { nullable: false })
+  @JoinColumn({ name: 'main_quiz_id' })
+  mainQuiz: MainQuiz;
+
+  @Column({ name: 'content', type: 'varchar', length: 255, nullable: true })
+  content?: string;
+
+  @Column({ name: 'answer', type: 'varchar', length: 255, nullable: true })
+  answer?: string;
+}
