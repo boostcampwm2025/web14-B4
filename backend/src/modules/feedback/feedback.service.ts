fffedb1ef9e0904ef15512fea8effc57642dcf85
standard-chan <starp321@naver.com>
2026-01-13 22:18:07 +0900

    feat: system 프롬프트 추가 및 함수로 분리
    - 시스템 프롬프트를 별도 constant 파일로 분리
    - analyzeAnswer 함수가 과도하게 길어서 user의 txt를 별도 함수로 분리.

--- backend/src/modules/feedback/feedback.service.ts
+++ backend/src/modules/feedback/feedback.service.ts
@@ -9,17 +9,25 @@ import { GoogleGenAI } from '@google/genai';
 import { CreateAIFeedbackRequestDto } from './dto/feedback-request.dto';
 import { MainQuiz } from 'src/datasources/entities/tb-main-quiz.entity';
 import { SpeechesService } from '../speeches/speeches.service';
+import {
+  AI_FEEDBACK_SYSTEM_PROMPT,
+  RESPONSE_SCHEMA,
+} from './constants/ai-constant';
 
 @Injectable()
 export class FeedbackService {
   private genAI: GoogleGenAI;
 
   constructor(
-    private mainQuizRepostory: MainQuizRepository,
+    private mainQuizRepository: MainQuizRepository,
     private speechesService: SpeechesService,
     private usersService: UsersService,
   ) {
-    this.genAI = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+    const apiKey = process.env.GEMINI_API_KEY;
+    if (!apiKey) {
+      throw new Error('GEMINI_API_KEY is missing');
+    }
+    this.genAI = new GoogleGenAI(apiKey);
   }
 
   async generateAIFeedback(
@@ -51,31 +59,16 @@ export class FeedbackService {
     return result;
   }
 
-  async analyzeAnswer(quizTitle: string, userAnswer: string) {
+  async analyzeAnswer(quizContent: string, userAnswer: string) {
     try {
       const response = await this.genAI.models.generateContent({
         model: 'gemini-2.5-flash-lite',
 
         config: {
-          systemInstruction: `
-            당신은 컴퓨터 과학(CS) 기술 면접관입니다. 
-            사용자의 답변을 분석하여 학습을 돕는 피드백을 제공하세요.
-            반드시 아래 JSON 스키마를 준수하여 응답하세요:
-            
-            {
-              "checklist": [
-                {"content": "핵심 키워드 포함 여부 (문장)", "isPass": boolean},
-                {"content": "개념의 정확한 정의 설명 여부 (문장)", "isPass": boolean}
-              ],
-              "feedback": "사용자에게 전하는 따뜻하고 구체적인 피드백 (경어체)",
-              "followUpQuestions": ["답변을 확장시킬 수 있는 꼬리질문 1", "꼬리질문 2", "꼬리질문 3"]
-            }
-          `,
-          temperature: 0.1,
+          systemInstruction: AI_FEEDBACK_SYSTEM_PROMPT,
           responseMimeType: 'application/json',
-          thinkingConfig: {
-            thinkingBudget: 0,
-          },
+          responseSchema: RESPONSE_SCHEMA,
+          temperature: 0.1,
         },
 
         contents: [
@@ -83,7 +76,7 @@ export class FeedbackService {
             role: 'user',
             parts: [
               {
-                text: `[퀴즈 내용]: ${quizTitle}\n[사용자 답변]: ${userAnswer}`,
+                text: this.createUserTxt(quizContent, userAnswer),
               },
             ],
           },
@@ -107,12 +100,31 @@ export class FeedbackService {
 
   private async validateQuiz(mainQuizId: number): Promise<MainQuiz> {
     const mainQuiz =
-      await this.mainQuizRepostory.findByIdWithDetails(mainQuizId);
+      await this.mainQuizRepository.findByIdWithDetails(mainQuizId);
 
     if (!mainQuiz) {
       throw new NotFoundException('해당 퀴즈가 존재하지 않습니다.');
     }
 
     return mainQuiz;
+  }
+
+  private createUserTxt(quizContent: string, userAnswer: string): string {
+    const txt = `
+    [퀴즈]
+    ${quizContent}
+
+    [사용자 답변]
+    ${userAnswer}
+
+    [사용자 체크리스트]
+    - (체크 항목 1): ✅ 또는 ❌
+    ...
+
+    [핵심 키워드 목록]
+    키워드1, 키워드2, 키워드3, ...
+    `;
+
+    return txt;
   }
 }
