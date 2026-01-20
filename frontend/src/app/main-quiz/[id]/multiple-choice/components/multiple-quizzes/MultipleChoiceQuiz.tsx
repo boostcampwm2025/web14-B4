aa4dcb65c2d71d288645112f54f2873e2d077676
standard-chan <starp321@naver.com>
2026-01-20 16:41:42 +0900

    style: container에 있던 items-center를 객관식 퀴즈 컴포넌트로 이동

--- frontend/src/app/main-quiz/[id]/multiple-choice/components/multiple-quizzes/MultipleChoiceQuiz.tsx
+++ frontend/src/app/main-quiz/[id]/multiple-choice/components/multiple-quizzes/MultipleChoiceQuiz.tsx
@@ -4,9 +4,9 @@ import Choice from './Choice';
 
 export default function MultipleChoiceQuiz() {
   return (
-    <div>
-      <h2 className="text-2xl font-semibold">
-        다음 중 스택에 대한 설명 중 옳지 않은 것을 고르시오
+    <div className='w-full flex flex-col items-center justify-center py-5'>
+      <h2 className="py-3 text-2xl font-semibold">
+        다음 중 스택과 큐에 대한 설명 중 옳지 않은 것을 고르세요.
       </h2>
       <div className="flex flex-col items-center gap-5 mt-6">
         <Choice />
