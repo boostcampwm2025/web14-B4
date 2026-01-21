e16f7a8f940534f1ee77aa6f7cbf02523023b09f
Hyerin Park <phl2510@naver.com>
2026-01-21 22:26:35 +0900

    fix: auth 관련 dto에 데코레이터 추가
    - main.ts에서 dto 클래스에 데코레이터가 없는 속성은 모두 제거됩니다.
    - 제거된 속성이 요청에 포함되어 있으면 에러가 발생합니다.
    - NaverLoginDto에 검증 데코레이터가 없었기 때문에 이를 추가했습니다.

--- backend/src/modules/auth/dto/naver-login.ts
+++ backend/src/modules/auth/dto/naver-login.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class NaverLoginDto {
+  @IsString()
+  @IsNotEmpty()
   code: string;
+
+  @IsString()
+  @IsNotEmpty()
   state: string;
 }
