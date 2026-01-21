cd3fe44e6fe2f4355c6f12faab3e08f31d1ebd20
standard-chan <starp321@naver.com>
2026-01-21 17:03:35 +0900

    feat: 객관식 선지 문제 무작위로 섞는 로직 추가
    - 사용자가 순서로 답을 암기하는 상황을 막기 위해, 선지 순서를 무작위로
      섞는 로직을 추가하였습니다

--- frontend/src/app/main-quiz/[id]/multiple-choice/components/multiple-quizzes/MultipleChoiceQuiz.tsx
+++ frontend/src/app/main-quiz/[id]/multiple-choice/components/multiple-quizzes/MultipleChoiceQuiz.tsx
@@ -2,6 +2,8 @@
 
 import { MultipleChoice } from '@/types/multipleChoice.types';
 import Choice from './Choice';
+import { useMemo } from 'react';
+import { shuffle } from '@/utils/shuffle';
 
 interface MultipleQuizProps {
   multipleChoiceQuizzes: MultipleChoice[];
@@ -14,18 +16,19 @@ export default function MultipleChoiceQuiz({
 }: MultipleQuizProps) {
   const currentQuiz = multipleChoiceQuizzes[quizIndex];
 
-  if (!currentQuiz) {
-    return null;
-  }
+  const shuffledOptions = useMemo(() => {
+    if (!currentQuiz || !currentQuiz.options) return [];
+    return shuffle([...currentQuiz.options]);
+  }, [currentQuiz]);
 
-  const { content, options } = currentQuiz;
+  const content = currentQuiz ? currentQuiz.content : '문제가 존재하지 않습니다.';
 
   return (
     <div className="w-full flex flex-col items-center justify-center py-2">
       <h2 className="max-w-3/4 py-2 my-5 text-2xl font-semibold leading-8">{content}</h2>
 
       <div className="flex flex-col items-center gap-5 mt-6">
-        {options.map((option) => (
+        {shuffledOptions.map((option) => (
           <Choice
             key={option.multipleQuizOptionId}
             text={option.option}
