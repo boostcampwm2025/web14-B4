5badacd5b51a33efa79236834d49f0b7320e3eae
Hyerin Park <phl2510@naver.com>
2026-01-21 21:57:52 +0900

    chore: auth 관련 모듈 의존성 구성 및 JWT 설정 추가
    - ConfigModule, JwtModule 비동기 설정 추가
    - 환경변수 기반 jwt 시크릿 만료 시간 설정
    - User엔티티 forFeature로 등록

---
+++ backend/src/modules/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../../datasources/entities/tb-user.entity';
+import { UserRepository } from '../../datasources/repositories/tb-user.repository';
+
+@Module({
+  imports: [
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, UserRepository],
+  exports: [AuthService],
+})
+export class AuthModule {}
