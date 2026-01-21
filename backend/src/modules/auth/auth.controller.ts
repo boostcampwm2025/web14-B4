93301855fdcb72640fd4881d2878af5854502915
Hyerin Park <phl2510@naver.com>
2026-01-22 00:57:43 +0900

    feat: auth 컨트롤러에 엔드포인트 추가

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { NaverLoginDto } from './dto/naver-login';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('login/naver')
   async loginNaver(@Body() dto: NaverLoginDto) {
     return await this.authService.loginWithNaver(dto);
+  }
+
+  @Post('refresh')
+  async refresh(@Body('refreshToken') dto: RefreshTokenDto) {
+    return await this.authService.refresh(dto.refreshToken);
   }
 }
