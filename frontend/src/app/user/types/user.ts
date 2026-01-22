1b1880ff9e8753e97613cedb9f629a361b73e257
jum0624 <jum0624@naver.com>
2026-01-22 14:30:57 +0900

    fix: 프로필 이메일 정보 제거

--- frontend/src/app/user/types/user.ts
+++ frontend/src/app/user/types/user.ts
@@ -1,7 +1,6 @@
 export interface UserProfile {
   id: string; // UUID를 문자열로 가져올 것
   name: string;
-  email: string;
   profileImage?: string;
   grade: 'bronze' | 'silver' | 'gold' | 'platinum';
   interests: string[]; // 관심분야
