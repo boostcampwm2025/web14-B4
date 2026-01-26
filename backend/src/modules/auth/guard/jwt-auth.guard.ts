e3360d2d55e402c3d3efb6a9243c6046f6cda535
Hyerin Park <phl2510@naver.com>
2026-01-26 17:03:11 +0900

    refactor: 이해에 도움이 되기 위한 주석 추가

--- backend/src/modules/auth/guard/jwt-auth.guard.ts
+++ backend/src/modules/auth/guard/jwt-auth.guard.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { BusinessException } from 'src/common/exceptions/business.exception';
 import { ERROR_MESSAGES } from 'src/common/constants/error-messages';
 
+// JWT 인증 담당 Guard(JwtStrategy를 실행해 토큰을 검증)
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   canActivate(
@@ -17,9 +18,11 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     user: unknown,
     info: unknown,
   ): TUser {
+    // 실패하면 에러 발생
     if (err || !user || info) {
       throw new BusinessException(ERROR_MESSAGES.ACCESS_DENIED);
     }
+    // 성공하면 user 반환
     return user as TUser;
   }
 }
