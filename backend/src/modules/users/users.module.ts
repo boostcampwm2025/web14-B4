0f6e30d8b7736a32ae9189c10c849fda9e3ba2ba
standard-chan <starp321@naver.com>
2026-01-27 14:44:00 +0900

    fix: users module에 quiz module 추가

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { DatasourcesModule } from 'src/datasources/datasources.module';
+import { QuizModule } from '../quizzes/quizzes.module';
 
 @Module({
-  imports: [DatasourcesModule],
+  imports: [DatasourcesModule, QuizModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
