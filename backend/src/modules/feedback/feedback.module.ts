1098702adb776fb4f1b627d10049cb43bbedd099
Hyerin Park <phl2510@naver.com>
2026-01-29 00:09:11 +0900

    refactor: feedback 모듈에 AuthModule 의존성 추가

--- backend/src/modules/feedback/feedback.module.ts
+++ backend/src/modules/feedback/feedback.module.ts
@@ -4,9 +4,10 @@ import { FeedbackService } from './feedback.service';
 import { DatasourcesModule } from 'src/datasources/datasources.module';
 import { SpeechesService } from '../speeches/speeches.service';
 import { UsersService } from '../users/users.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [DatasourcesModule],
+  imports: [DatasourcesModule, AuthModule],
   controllers: [FeedbackController],
   providers: [FeedbackService, SpeechesService, UsersService],
 })
