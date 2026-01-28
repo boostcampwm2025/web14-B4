bc4657b0b45af628b3e1fced568a41c4463f85ed
Hyerin Park <phl2510@naver.com>
2026-01-28 18:01:01 +0900

    refactor: 비로그인 사용자가 /user 접근 시 /quizzes로 리다이렉트

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -1,5 +1,6 @@
 export const dynamic = 'force-dynamic';
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import { getUserComprehensions, getUserSolvedStatistics } from '@/services/apis/usersApi';
 import QuizStatisticsSection from './components/QuizStatisticsSection';
 import MyProfile, { mockUserData } from './components/MyProfile';
@@ -7,6 +8,12 @@ import ImportanceBoard from './components/importanceGroup/ImportanceBoard';
 
 export default async function Page() {
   const cookieStore = await cookies();
+  // accessToken이 없으면 로그인 페이지로 리다이렉트
+  const accessToken = cookieStore.get('accessToken')?.value;
+  if (!accessToken) {
+    redirect('/quizzes?authRequired=true');
+  }
+
   const username = cookieStore.get('username')?.value || '사용자';
 
   const [comprehensionResponse, solvedQuizResponse] = await Promise.all([
