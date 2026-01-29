167b95af192f5c738eff5867039093c39a9c0cf5
J <205630477+sw-dev-j@users.noreply.github.com>
2026-01-29 21:59:51 +0900

    refactor: footer 문구 수정

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,27 +1,39 @@
 'use client';
 
 import Link from 'next/link';
-import Image from 'next/image';
 
 export default function Footer() {
   return (
     <footer className="w-full border-t bg-[var(--color-bg-default)]">
-      <div className="mx-auto flex h-14 max-w-screen-xl items-center justify-center px-4 text-sm">
-        <div className="flex items-center gap-5">
-          <span>© CS뽁뽁 - CS 개념을 말로 설명하며 스스로 사고하는 학습을 돕는 서비스</span>
+      <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center gap-2 px-4 py-6 text-center text-sm text-gray-400">
+        {/* 서비스 소개 문구 */}
+        <p className="whitespace-nowrap">
+          CS뽁뽁 - CS 개념을 말로 설명하며 스스로 사고하는 학습을 돕는 서비스
+        </p>
+
+        {/* 하단 링크 영역 */}
+        <div className="flex items-center gap-2">
+          <span>© boostcamp2025 WEB14 B4 Team</span>
+          <span>/</span>
+
+          <Link
+            href="https://github.com/boostcampwm2025/web14-B4" // 추후 서비스 소개 페이지 연결
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            서비스 소개
+          </Link>
+
+          <span>/</span>
 
           <Link
             href="https://github.com/boostcampwm2025/web14-B4"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:scale-110"
+            className="hover:underline"
           >
-            <Image
-              src="/icons/icon-GitHub_Invertocat_Black_Clearspace.svg"
-              alt="GitHub 저장소로 이동하기"
-              width={40}
-              height={40}
-            />
+            GitHub
           </Link>
         </div>
       </div>
